a0730838d7fdf6f26e49b702593057b85116016f
Echo <yhyecho@gmail.com>
2017-07-19 14:57:51 +0800

    add cats add and cats list code

--- client/src/components/NewCat.js
+++ client/src/components/NewCat.js
@@ -16,6 +16,21 @@ class NewCat extends React.Component {
         cats: res.data.cats
       })
     })
+  }
+  _updateCatList() {
+    axios.get(`${config.host}/cats`)
+      .then((res) => this.setState({cats: res.data.cats}))
+      .catch(err => console.log(err))
+  }
+  _handleSubmit(e) {
+    e.preventDefault()
+    let catName = this.refs.catName.value
+    this.refs.catName.value = ''
+    let data = {name: catName}
+    axios.post(`${config.host}/category`, data)
+      .then((res) => {
+        this._updateCatList()
+      })
   }  
   render() {
     let catList = this.state.cats.map((item, i) => {
@@ -27,7 +42,13 @@ class NewCat extends React.Component {
     })
     return (
       <div>
-        {catList}
+        <ul>
+          {catList}
+        </ul>
+        <form onSubmit={this._handleSubmit.bind(this)}>
+          <input ref='catName' type='text' />
+          <input type='submit' />
+        </form>
       </div>
     )
   }
